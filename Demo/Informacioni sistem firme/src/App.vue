a923c5b82b402099d0991b0d3613b4fe419391f3
Sloba98 <slobacar635@gmail.com>
2023-12-24 10:51:33 +0100

    Dodata je inicijalna struktura za demo i prva stranica

---
+++ Demo/Informacioni sistem firme/src/App.vue
@@ -0,0 +1,21 @@
+
+
+<template>
+<kreiranje-zahteva></kreiranje-zahteva>
+
+</template>
+
+<style scoped>
+
+</style>
+
+
+<script>
+import KreiranjeZahteva from './components/Kreiranje-Zahteva.vue';
+
+export default {
+  components: {
+    KreiranjeZahteva,
+  },
+};
+</script>
---
+++ Demo/Informacioni sistem firme/src/router/index.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import App from '../App.vue';
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: App,
+    },
+  ]
+})
+
+export default router
